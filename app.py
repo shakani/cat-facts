9031231b65d9b84b4c15aa31e5a8794217d1f943
Sami Hakani <sami.hakani@gmail.com>
2023-11-08 16:27:36 -0500

    Fetched cat pic and removed catfact placeholder

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 import streamlit as st 
 import catfact as cf 
-
-def fetch_fact():
-    return 'Cat fact placeholder'
+import catpic as cp
 
 def app():
     st.title('Cat Facts')
     st.markdown("Here's a random cat fact to brighten your day!")
     fact = cf.fetch_fact()
     st.markdown(fact)
+    img = cp.fetch_pic()
+    st.image(img)
 
 if __name__ == "__main__":
     app()
