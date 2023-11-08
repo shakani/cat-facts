30ae470074b04d2ce36fb9f2b83b2befd2f6f014
Sami Hakani <sami.hakani@gmail.com>
2023-11-08 16:16:01 -0500

    Created method to generate fact

---
+++ catfact.py
@@ -0,0 +1,11 @@
+import requests
+import json 
+
+def fetch_fact() -> str:
+    """
+    Generates and returns random cat fact.
+    """
+    pass 
+
+if __name__ == "__main__":
+    print(fetch_fact())
