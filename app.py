ff537388fbdccc565c419ec3a7d1014e3ae3015f
Sami Hakani <sami.hakani@gmail.com>
2023-11-08 16:29:35 -0500

    Added flag to use API for fetch methods

--- app.py
+++ app.py
@@ -2,12 +2,27 @@ import streamlit as st
 import catfact as cf 
 import catpic as cp
 
+USE_API = False
+
+def fetch_fact(USE_API=False):
+    if not USE_API:
+        return 'Cat fact'
+    else:
+        return cf.fetch_fact()
+
+def fetch_pic(USE_API=False):
+    if not USE_API:
+        return 'https://cdn2.thecatapi.com/images/MTYwNzYxNg.jpg'
+    else:
+        return cp.fetch_pic()
+
+
 def app():
     st.title('Cat Facts')
     st.markdown("Here's a random cat fact to brighten your day!")
-    fact = cf.fetch_fact()
+    fact = fetch_fact(USE_API)
     st.markdown(fact)
-    img = cp.fetch_pic()
+    img = fetch_pic(USE_API)
     st.image(img)
 
 if __name__ == "__main__":
